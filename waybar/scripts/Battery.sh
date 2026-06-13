2495a4e0b0279bd1b2ab347f4278e3da5156f63d
Sharwin-sriram <shreeramtooo@gmail.com>
2026-06-13 21:47:29 +0530

    Create: Script to fetch battery levels Update: waybar - increase visiblity

---
+++ waybar/scripts/Battery.sh
@@ -0,0 +1,16 @@
+charge=$(cat /sys/class/power_supply/BAT0/capacity)
+status=$(cat /sys/class/power_supply/BAT0/status)
+
+if [ "$status" = "Charging" ]; then
+    class="charging"
+elif [ "$capacity" -le 10 ]; then
+    class="critical"
+elif [ "$capacity" -le 30 ]; then
+    class="low"
+elif [ "$capacity" -le 60 ]; then
+    class="medium"
+else
+    class="high"
+fi
+
+printf '{"text": "%d","class": "%s"}\n' "$charge" "$class"
