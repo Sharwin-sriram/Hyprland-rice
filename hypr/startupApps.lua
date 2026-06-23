eb5340b772f1cb5840bc61745dca9f7369e5da5d
Sharwin-sriram <shreeramtooo@gmail.com>
2026-06-23 15:09:40 +0530

    Update: Add hypridle in startup apps config and update hyprlock to a more minimalistic setup

--- hypr/startupApps.lua
+++ hypr/startupApps.lua
@@ -1,5 +1,6 @@
 hl.on("hyprland.start", function ()
     hl.exec_cmd("hyprpaper")
+    hl.exec_cmd("hypridle")
     hl.exec_cmd("systemctl --user start graphical-session.target")
     hl.exec_cmd("systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
     hl.exec_cmd("waybar -c $HOME/.config/waybar/config-top.jsonc -s $HOME/.config/waybar/style-top/style-top.css")
