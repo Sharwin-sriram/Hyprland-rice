a181aec60b1b44f7ef3454ea8f4643715661018e
Sharwin-sriram <shreeramtooo@gmail.com>
2026-06-29 09:34:49 +0530

    Chore:

--- hypr/scripts/Weather/WeatherDetails.sh
+++ hypr/scripts/Weather/WeatherDetails.sh
@@ -6,7 +6,6 @@ LOCATION=$("$HOME/.config/hypr/scripts/Weather/Location.sh")
 LAT=$(echo "$LOCATION" | jq -r '.loc' | cut -d',' -f1)
 LON=$(echo "$LOCATION" | jq -r '.loc' | cut -d',' -f2)
 
-
 WEATHER=$(curl -s "https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&appid=${OPENWEATHER_API_KEY}&units={metric}")
 
 NAME=$(echo "$WEATHER" | jq -r '.name')
