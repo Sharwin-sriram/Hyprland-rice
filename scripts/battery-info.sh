9549d7dd882c444d6e7a95f6d9ddac5220169d9c
Sharwin-sriram <shreeramtooo@gmail.com>
2026-07-17 10:24:04 +0530

    Create: script to kill hyprland and more battery info

--- scripts/battery-info.sh
+++ scripts/battery-info.sh
@@ -2,8 +2,10 @@ percentage=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/perc
 cycles=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/charge-cycles/ {print $2}')
 full_design=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/energy-full-design/ {print $2}')
 full_now=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/energy-full:/ {print $2}')
+full_factory=87.4
 
 printf 'Percentage: %s\n' $percentage
 printf 'Charge cycles: %s\n' $cycles
+printf 'Battery capacity factory: %s Wh\n' $full_factory
 printf 'Battery capacity full: %s Wh\n' $full_now
 printf 'Battery design capacity: %s Wh\n' $full_design
