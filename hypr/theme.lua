eafc51d199e774a7b05459cd89a812f143a06d53
Sharwin-sriram <shreeramtooo@gmail.com>
2026-07-25 11:15:22 +0530

    Chore: themes

--- hypr/theme.lua
+++ hypr/theme.lua
@@ -1,8 +1,8 @@
 hl.config({
 	general = {
 		col = {
-			active_border = "rgb(252,183,123)",
-			inactive_border = "rgb(32,70,70)",
+			active_border = "rgb(241,238,248)",
+			inactive_border = "rgb(110,146,58)",
 		},
 	},
 })
